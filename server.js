bb36a932ebe5dd91c3c5f86dc5f2ef6dd62db0e4
rinasao <rinasaobit@gmail.com>
2020-09-26 13:00:15 +0530

    added server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+var bodyparser = require('body-parser')
+const Port = process.env.Port || 9000;
+const server = express();
+
+server.use(bodyparser.json())
+server.use(express.static(__dirname + '/client'))
+server.get('/', function(req, res){
+    console.log("base route");
+    res.sendFile(__dirname+'/client/home.html')
+})
+
+server.listen(Port, function () {
+    console.log("Server is running at...", Port);
+  });
